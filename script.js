03f3f6e996dea73e94f42eeb877a7b9e44a6f62a
GM2 Academy <gm2academy@Tecnicos-MacBook-Pro.local>
2023-08-08 11:14:22 -0300

    feat(script.js): constructor & function

--- script.js
+++ script.js
@@ -1,25 +1,51 @@
-class Auto {
-    constructor(name, price, type, kms) {
-        this.name = name
-        this.price = price
-        this.type = type
-        this.kms = kms
-    }
+// class Auto {
+//     constructor(name, price, type, kms) {
+//         this.name = name
+//         this.price = price
+//         this.type = type
+//         this.kms = kms
+//     }
+// }
+// let listProduct = [];
+
+// const bmw = new Auto(
+//     'BMW E46 M3', 29990, "sedan", 506
+// )
+// const mercedes_benz = new Auto(
+//     'mercedes_benz', 1539050, "fastback", 106
+// )
+// const audi = new Auto(
+//     'audi', 158600, "sedan", 5843
+// )
+// listProduct.push(bmw)
+// listProduct.push(mercedes_benz)
+// listProduct.push(audi)
+// console.log(listProduct)
+
+
+function createAuto(name, price, type, kms) {
+    return {
+        name: name,
+        price: price,
+        type: type,
+        kms: kms
+    };
 }
+
 let listProduct = [];
 
-const bmw = new Auto(
+const bmw = createAuto(
     'BMW E46 M3', 29990, "sedan", 506
-)
-const mercedes_benz = new Auto(
+);
+const mercedes_benz = createAuto(
     'mercedes_benz', 1539050, "fastback", 106
-)
-const audi = new Auto(
+);
+const audi = createAuto(
     'audi', 158600, "sedan", 5843
-)
-listProduct.push(bmw)
-listProduct.push(mercedes_benz)
-listProduct.push(audi)
-console.log(listProduct)
+);
 
+listProduct.push(bmw);
+listProduct.push(mercedes_benz);
+listProduct.push(audi);
 
+console.log(listProduct);
