40d6443b3b1660e9fc36111985b4f9cffb546ea9
GM2 Academy <gm2academy@Tecnicos-MacBook-Pro.local>
2023-08-11 11:10:01 -0300

    feat(login): login to index movement

---
+++ login/login.js
@@ -0,0 +1,5 @@
+// Agrega un evento de clic al botón
+document.getElementById("submit-button").addEventListener("click", function() {
+    // Cambia la ubicación a la nueva página .html
+    window.location.href = "../index.html"; // Reemplaza "nuevo-archivo.html" con la ruta correcta
+});
