be1909349daddeff95edda0b34117c221ce1ed00
GM2 Academy <gm2academy@Tecnicos-MacBook-Pro.local>
2023-08-17 11:22:46 -0300

    feat(login.js): input verification

--- login/login.js
+++ login/login.js
@@ -1,32 +1,61 @@
-document.getElementById("submit-button").addEventListener("click", function () {
+document.addEventListener("DOMContentLoaded", function () {
     const usernameInput = document.getElementById("username");
     const passwordInput = document.getElementById("password");
+    const submitButton = document.getElementById("submit-button");
 
-    const username = usernameInput.value;
-    const password = passwordInput.value;
+    usernameInput.addEventListener("keyup", function (event) {
+        if (event.key === "Enter") {
+            passwordInput.focus();
+        }
+    });
 
-    if (username === "" || password === "") {
-        alert("Please fill in both username and password fields.");
-        return;
+    passwordInput.addEventListener("keyup", function (event) {
+        if (event.key === "Enter") {
+            handleLogin();
+        }
+    });
+
+    submitButton.addEventListener("keyup", function (event) {
+        if (event.key === "Enter") {
+            handleLogin();
+        }
+    });
+
+    submitButton.addEventListener("click", handleLogin);
+
+    function handleLogin() {
+        const username = usernameInput.value;
+        const password = passwordInput.value;
+
+        if (username === "" || password === "") {
+            showAlert("Please fill in both username and password fields.");
+            return;
+        }
+
+        const users = [
+            {
+                username: "admin", password: "1234"
+            },
+            {
+                username: "pepe", password: "el"
+            },
+            {
+                username: "pepesand", password: "papadeindependiente"
+            },
+        ];
+
+        const userFound = users.find(user => user.username === username && user.password === password);
+
+        if (userFound) {
+            window.location.href = "../index.html";
+        } else {
+            showAlert("Invalid username or password.");
+            usernameInput.focus();
+            passwordInput.value = ""; 
+        }
     }
 
-    const users = [
-        {
-            username: "admin", password: "1234"
-        },
-        {
-            username: "pepe", password: "el"
-        },
-        {
-            username: "pepesand", password: "papadeindependiente"
-        },
-    ];
-
-    const userFound = users.find(user => user.username === username && user.password === password);
-
-    if (userFound) {
-        window.location.href = "../index.html";
-    } else {
-        alert("Invalid username or password.");
+    function showAlert(message) {
+        alert(message);
     }
 });
