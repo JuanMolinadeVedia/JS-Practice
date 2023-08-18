2348169cb1ca867b6a10fa2820c561a898b05793
GM2 Academy <gm2academy@Tecnicos-MacBook-Pro.local>
2023-08-18 11:31:04 -0300

    feat(index): dark mode

--- script.js
+++ script.js
@@ -79,3 +79,16 @@ function updateProductCards() {
 //  addProductButton.addEventListener("click", function () {
 //      addProduct(products);  
 //  });
+const darkModeToggle = document.getElementById("darkModeToggle");
+const body = document.body;
+
+darkModeToggle.addEventListener("change", function () {
+    if (darkModeToggle.checked) {
+        body.classList.add("dark-mode");
+        
+    } else {
+        body.classList.remove("dark-mode");
+    }
+    console.log(darkModeToggle.checked);
+
+});
